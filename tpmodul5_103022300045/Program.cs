7128877c61d6f7bede69c4dca37b14bbf175013e
ZakyAmarullah <zakym0208@gmail.com>
2025-03-16 21:26:44 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_103022300045/Program.cs
+++ tpmodul5_103022300045/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo User " + user);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Zaky");
+    }
+}
